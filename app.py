3a0f96004a435866265bfe46502f1ff79f1968eb
haroonomer <haroonomer2000@yahoo.com>
2021-09-04 21:20:18 +0500

    update file

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 
 from flask import Flask, jsonify, Blueprint, request
+from flask.helpers import send_from_directory
+from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='frontend/build', static_url_path='')
+
+CORS(app)
 
 
 @app.route('/api', methods=['GET'])
+@cross_origin()
 def index():
 
     return {
         'tutorial': 'Flask APP Heroku'
     }
+
+
+@app.route('/')
+@cross_origin()
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
 
 
 if __name__ == '__main__':
